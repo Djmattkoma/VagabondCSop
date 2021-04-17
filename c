72963de4f181ee4a4b717f95f5bc394b53d35f0b
Djmattkoma <55582553+Djmattkoma@users.noreply.github.com>
2021-04-18 00:13:11 +0200

    90%

---
+++ c
@@ -0,0 +1,18 @@
+<div class="hackbanner" id="hack-1">
+    <div class="overlay"></div>
+    <div class="content">
+      <div class="close-btn" onclick="togglePopup()">&times;</div>
+      <h1>FIZES MOST!!</h1>
+      <h2>külömben minden adatot törlünk!</h2>
+          <p id="szv">utalás ide: x*wfdg-afv442</p>
+          <p id="nwtime">adatok törlése # timer</p>
+    </div>
+  </div>
+
+  <div class="hackbanner2" id="hack-2">
+    <div class="overlay"></div>
+    <div class="content">
+      <i class="fa fa-database" style="font-size:200px;color:red"></i>
+      <div id="hacklodbar"></div>
+    </div>
+  </div>
--- script/animation.js
+++ script/animation.js
@@ -1,28 +1,68 @@
 function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-  }
-
-function hackdonw() {
-  
+  document.getElementById("").classList.toggle("active");
+  countdonw()
+  progres()
 }
 
-function progres(params) {
-  var i = 0;
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("lodbar");
-    var wth = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (wth >= 100) {
-        clearInterval(id)
-        i = 0;
-      }else{
-        wth++;
-        elem.style.width =  wth + "%";
-        elem.innerHTML = wth + "%";
-      }
+function countdonw() {
+  var date;
+    var display = document.getElementById('nwtime');
+    setInterval(function(){
+        date = new Date();
+        var currenthours = date.getHours();
+        var hours;
+        var minutes;
+        var secondes;
+        if (currenthours != 23){
+            if (currenthours < 23)
+                hours = 47 - currenthours;
+            else hours = 47 + (23 - currenthours);
+            minutes = 60 - date.getMinutes();
+            secondes = 60 - date.getSeconds();
+        display.innerHTML = hours + 'h ' + minutes + 'm ' +secondes + 's';
+        }
+        else display.innerHTML = 'DATAS DELETED!';
+    },1000);
+}
+
+countdonw()
+
+function progres() {
+  var elem = document.getElementById("hacklodbar2");
+  var wth = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (wth >= 100) {
+      clearInterval(id);
+    }else{
+      wth++;
+      elem.style.width =  wth + "%";
+      document.getElementById("hacklodbar").innerHTML = "DATABASE HACKED"
     }
   }
 }
-          
+
+
+function x1() {
+  var code1 = Math.floor((Math.random() * 10) + 1);
+  var code2 = Math.floor((Math.random() * 10) + 1);
+  var code3 = Math.floor((Math.random() * 10) + 1);
+
+  if (tip1 == code1) {
+    document.getElementById(led1).innerHTML.style.background = lime
+  }else{
+    document.getElementById(led1).innerHTML.style.background = red;
+  }
+
+  if (tip2 == code2) {
+    document.getElementById(led1).innerHTML.style.background = lime
+  }else{
+    document.getElementById(led1).innerHTML.style.background = red;
+  }
+  
+  if (tip3 == code3) {
+    document.getElementById(led1).innerHTML.style.background = lime
+  }else{
+    document.getElementById(led1).innerHTML.style.background = red;
+  }
+} 
